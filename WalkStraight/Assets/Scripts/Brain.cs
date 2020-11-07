7e86abc598b5fae16510f263403d5ba33f3c28b0
Mattias Strömberg <m.stromberg@outlook.com>
2020-11-07 14:14:07 +0100

    Coding Movement with Genes Part 1

---
+++ WalkStraight/Assets/Scripts/Brain.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class Brain : MonoBehaviour
+{
+    public int DNALength { get; set; }
+    public float TimeAlive { get; set; }
+    public DNA DNA { get; set; }
+
+    private ThirdPersonCharacter _character;
+    private Vector3 _move;
+    private bool _jump;
+    private bool _alive = true;
+
+    public void Init()
+    {
+        // Initialize DNA
+        // 0 Forward
+        // 1 back
+        // 2 left
+        // 3 right
+        // 4 jump
+        // 5 crouch
+
+        TimeAlive = 0;
+        DNA = new DNA(DNALength, 6);
+        _character = GetComponent<ThirdPersonCharacter>();
+        _alive = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "dead")
+        {
+            _alive = false;
+        }
+    }
+
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        // Read DNA
+        float vertical = 0;
+        float horizontal = 0;
+        bool crouch = false;
+
+        if (DNA.GetGene(0) == 0) vertical = 1;
+        else if (DNA.GetGene(0) == 1) vertical = -1;
+        else if (DNA.GetGene(0) == 2) horizontal = -1;
+        else if (DNA.GetGene(0) == 3) horizontal = 1;
+        else if (DNA.GetGene(0) == 4) _jump = true;
+        else if (DNA.GetGene(0) == 5) crouch = true;
+
+        _move = vertical * Vector3.forward + horizontal * Vector3.right;
+        _character.Move(_move, crouch, _jump);
+        _jump = false;
+
+        if (_alive)
+        {
+            TimeAlive += Time.deltaTime;
+        }
+    }
+}
---
+++ WalkStraight/Assets/Scripts/DNA.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour
+{
+    private List<int> _genes = new List<int>();
+    private int _dnaLength = 0;
+    private int _maxValues = 0;
+
+    public DNA(int length, int values)
+    {
+        _dnaLength = length;
+        _maxValues = values;
+
+        SetRandom();
+    }
+
+    private void SetRandom()
+    {
+        _genes.Clear();
+
+        for (int i = 0; i < _dnaLength; i++)
+        {
+            _genes.Add(Random.Range(0, _maxValues));
+        }
+    }
+
+    public void SetInt(int index, int value)
+    {
+        _genes[index] = value;
+    }
+
+    public void Combine(DNA dna1, DNA dna2)
+    {
+        for (int i = 0; i < _dnaLength; i++)
+        {
+            if (i < _dnaLength / 2.0f)
+            {
+                int c = dna1._genes[i];
+                _genes[i] = c;
+            }
+            else
+            {
+                int c = dna2._genes[i];
+                _genes[i] = c;
+            }
+        }
+    }
+
+    public void Mutate()
+    {
+        _genes[Random.Range(0, _dnaLength)] = Random.Range(0, _maxValues);
+    }
+
+    public int GetGene(int index)
+    {
+        return _genes[index];
+    }
+}
